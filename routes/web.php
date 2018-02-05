e4e193e224df117a523b35aefdd6a47fef06b768
linxiaolong <1394139539@qq.com>
2018-02-05 14:25:15 +0800

    laravel-debugbar包的使用: 以及理解 composer install --no-dev 的作用

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,34 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// laravel-debugbar 扩展使用案例
+Route::get('/laravel_debugbar', function () {
+    // 信息: 自定义添加调试信息
+    $object = 'object';
+    Debugbar::info($object);
+    Debugbar::error('Error!');
+    Debugbar::warning('Watch out…');
+    Debugbar::addMessage('Another message', 'mylabel');
+
+    // 时间轴: 新增一个计时
+    Debugbar::startMeasure('render','Time for rendering');
+    sleep(1);
+    Debugbar::stopMeasure('render');
+
+    // 异常记录到调试栏
+    try {
+        throw new \Exception('foobar');
+    } catch (\Exception $e) {
+        Debugbar::addThrowable($e);
+    }
+
+    // 自定义注入 js, 先获取
+    $renderer = Debugbar::getJavascriptRenderer();
+
+    // 运行时开关
+//    Debugbar::enable();
+//    Debugbar::disable();
+
+    return view('welcome');
+});
