ac84e1a316211d7285e2c508ce4274984b9635e6
nounen <vagrant@homestead>
2019-03-04 01:51:49 +0000

    seeder 填充更多用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Seeder;
 /**
  * Class UsersTableSeeder
  *
- * 填充一个测试用户 admin/admin
+ * 填充测试用户 admin/admin
  */
 class UsersTableSeeder extends Seeder
 {
@@ -16,9 +16,16 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $userName = 'admin';
-        $userPassword = 'admin';
-        $userEmail = 'admin@qq.com';
+        foreach ($this->getUsers() as $user) {
+            $this->insertUser($user);
+        }
+    }
+
+    protected function insertUser($user)
+    {
+        $userName = $user['name'];
+        $userPassword = $user['password'];
+        $userEmail = $user['mail'];
 
         $user = $this->userTable()
             ->where('name', $userName)
@@ -31,6 +38,62 @@ class UsersTableSeeder extends Seeder
                 'password' => bcrypt($userPassword),
             ]);
         }
+    }
+
+    protected function getUsers()
+    {
+        return [
+            [
+                'name' => 'admin',
+                'password' => 'admin',
+                'mail' => 'admin@qq.com',
+            ],
+            [
+                'name' => 'test1',
+                'password' => 'test1',
+                'mail' => 'test1@qq.com',
+            ],
+            [
+                'name' => 'test2',
+                'password' => 'test2',
+                'mail' => 'test2@qq.com',
+            ],
+            [
+                'name' => 'test3',
+                'password' => 'test3',
+                'mail' => 'test3@qq.com',
+            ],
+            [
+                'name' => 'test4',
+                'password' => 'test4',
+                'mail' => 'test4@qq.com',
+            ],
+            [
+                'name' => 'test5',
+                'password' => 'test5',
+                'mail' => 'test5@qq.com',
+            ],
+            [
+                'name' => 'test6',
+                'password' => 'test6',
+                'mail' => 'test6@qq.com',
+            ],
+            [
+                'name' => 'test7',
+                'password' => 'test7',
+                'mail' => 'test7@qq.com',
+            ],
+            [
+                'name' => 'test8',
+                'password' => 'test8',
+                'mail' => 'test8@qq.com',
+            ],
+            [
+                'name' => 'test9',
+                'password' => 'test9',
+                'mail' => 'test9@qq.com',
+            ],
+        ];
     }
 
     protected function userTable()
